795cf1cc4f225ac91a6cf53bb4a078c23aee82cc
Kevin Murphy <locustshadow@gmail.com>
2025-12-19 10:55:38 -0700

    Initial commit - Spring Boot GitHub service

---
+++ src/test/java/com/branch/service/github/GithubServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.branch.service.github;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
